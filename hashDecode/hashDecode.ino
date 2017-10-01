ecd28bb89a27cf119133e0d56f2ddbcea4a3d3c1
Martino Facchin <m.facchin@arduino.cc>
2017-10-01 18:46:18 +0200

    add IR receive sketch

---
+++ hashDecode/hashDecode.ino
@@ -0,0 +1,54 @@
+/* hashDecode.ino Example sketch for IRLib2
+   Instead of decoding using a standard encoding scheme
+   (e.g. Sony, NEC, RC5), the code is hashed to a 32-bit value.
+   This should produce a unique 32-bit number however that number
+   cannot be used to retransmit the same code. This is just a quick
+   and dirty way to detect a unique code for controlling a device
+   when you don't really care what protocol or values are being sent.
+*/
+//First will create a decoder that handles only NEC, Sony and the hash
+// decoder. If it is not NEC or Sony that it will return a 32 bit hash.
+#include <IRLibDecodeBase.h>
+#include <IRLib_P01_NEC.h>
+#include <IRLib_P02_Sony.h>
+#include <IRLib_HashRaw.h>  //Must be last protocol
+#include <IRLibCombo.h>     // After all protocols, include this
+// All of the above automatically creates a universal decoder
+// class called "IRdecode" containing only the protocols you want.
+// Now declare an instance of that decoder.
+IRdecode myDecoder;
+
+// Include a receiver either this or IRLibRecvPCI or IRLibRecvLoop
+#include <IRLibRecv.h>
+IRrecv myReceiver(2);    //create a receiver on pin number 2
+
+void setup() {
+  Serial.begin(9600);
+Serial1.begin(115200);
+  myReceiver.enableIRIn(); // Start the receiver
+  Serial.println(F("Ready to receive IR signals"));
+}
+
+String decoded_string = "";
+int i = 0;
+
+void loop() {
+  if (myReceiver.getResults()) {
+    myDecoder.decode();
+    if (myDecoder.protocolNum == UNKNOWN) {
+    } else {
+      //myDecoder.dumpResults(false);
+      if (i % 3 == 0) {
+        decoded_string += (char)myDecoder.value;
+      }
+      i++;
+    };
+    if ((char)myDecoder.value == '\n') {
+      Serial.println(decoded_string);
+      Serial1.println(decoded_string);
+      decoded_string = "";
+    }
+    myReceiver.enableIRIn();
+  }
+}
+
